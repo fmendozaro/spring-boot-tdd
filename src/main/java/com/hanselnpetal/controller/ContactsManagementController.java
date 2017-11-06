e4e43f60122255f9d883fad3c16de80d850a8ccc
Fernando <fernandoza@gmail.com>
2017-11-06 12:12:15 -0600

    Added the rest of the controller implementation and the happy path for the first test, using a mockMVC perform request.

--- src/main/java/com/hanselnpetal/controller/ContactsManagementController.java
+++ src/main/java/com/hanselnpetal/controller/ContactsManagementController.java
@@ -2,30 +2,46 @@ package com.hanselnpetal.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.hanselnpetal.domain.ContactImportantOccasion;
 import com.hanselnpetal.domain.CustomerContact;
 import com.hanselnpetal.service.ContactsManagementService;
 
 @Controller
-@RequestMapping("/addContact")
 public class ContactsManagementController {
 
     @Autowired
     private ContactsManagementService contactsManagementService;
 
 
-    @RequestMapping(method=RequestMethod.POST)
-    public String processAddContactSubmit(@RequestBody CustomerContact aContact) {
+    @RequestMapping(value= "/addContact", method=RequestMethod.POST)
+    public String processAddContactSubmit(@ModelAttribute CustomerContact aContact) {
 
         CustomerContact newContact = contactsManagementService.add(aContact);
 
         if (newContact != null) {
-            return "success";
+            return "/addContactForm";
         }
 
-        return "failure";
+        return "redirect:/showAddContact";
+    }
+
+    @RequestMapping(value= "/showAddContact", method=RequestMethod.GET)
+    public String showAddContact() {
+
+        // implement this
+
+        return "/addContactForm";
+    }
+
+    public String processAddContactOccasionSubmit(@ModelAttribute CustomerContact aContact,
+                                                  @ModelAttribute ContactImportantOccasion anOccasion) {
+
+        // implement this
+
+        return "/addContactOccasionForm";
     }
 }
