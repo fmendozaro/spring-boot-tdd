535bae581580ba97e68037f0ccc16736ad36a0ee
Fernando <fernandoza@gmail.com>
2017-11-06 16:10:48 -0600

    Rolls back to the previous version of the controller.

--- src/main/java/com/hanselnpetal/controller/ContactsManagementController.java
+++ src/main/java/com/hanselnpetal/controller/ContactsManagementController.java
@@ -2,46 +2,30 @@ package com.hanselnpetal.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.hanselnpetal.domain.ContactImportantOccasion;
 import com.hanselnpetal.domain.CustomerContact;
 import com.hanselnpetal.service.ContactsManagementService;
 
 @Controller
+@RequestMapping("/addContact")
 public class ContactsManagementController {
 
     @Autowired
     private ContactsManagementService contactsManagementService;
 
 
-    @RequestMapping(value= "/addContact", method=RequestMethod.POST)
-    public String processAddContactSubmit(@ModelAttribute CustomerContact aContact) {
+    @RequestMapping(method=RequestMethod.POST)
+    public String processAddContactSubmit(@RequestBody CustomerContact aContact) {
 
         CustomerContact newContact = contactsManagementService.add(aContact);
 
         if (newContact != null) {
-            return "/addContactForm";
+            return "success";
         }
 
-        return "redirect:/showAddContact";
-    }
-
-    @RequestMapping(value= "/showAddContact", method=RequestMethod.GET)
-    public String showAddContact() {
-
-        // implement this
-
-        return "/addContactForm";
-    }
-
-    public String processAddContactOccasionSubmit(@ModelAttribute CustomerContact aContact,
-                                                  @ModelAttribute ContactImportantOccasion anOccasion) {
-
-        // implement this
-
-        return "/addContactOccasionForm";
+        return "failure";
     }
 }
